63d4a312162ff10312ee58ba866d0808e11e0e85
Abad Vera <abad_vera@me.com>
2026-02-05 23:50:38 -0800

    Add missing docstrings to utility.hpp

--- src/utility.cpp
+++ src/utility.cpp
@@ -5,6 +5,8 @@ namespace util {
 
 uint32_t rgbToUint32(uint8_t r, uint8_t g, uint8_t b)
 {
+    // Setting alpha to full opacity ----------|
+    //                                         v
     return (r << 24) | (g << 16) | (b << 8) | 0xFF;
 }
 
